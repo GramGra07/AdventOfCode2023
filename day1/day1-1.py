915d10a9a7f9b7ec8a12daf206ccdab6d3f4d86e
Graden <101433010+GramGra07@users.noreply.github.com>
2023-12-01 16:00:49 -0700

    day one complete

---
+++ day1/day1-1.py
@@ -0,0 +1,43 @@
+from colorama import Fore
+fileName = "day1-1Input.txt"
+file = open(fileName, "r")
+lines = []
+for line in file.readlines():
+    lines.append(line)
+file.close()
+
+numbers = ['one','two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+realNumbers = ['1','2','3','4','5','6','7','8','9']
+numbersDict = {'one':'1','two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8','nine':'9'}
+calibration_values = []
+for line in lines:
+    numberLine = ''
+    numLength = 0
+    numLengthR = 0
+    left = 1000000
+    right = 0
+    for num in realNumbers:
+        if num in line:
+            if line.find(num) < left:
+                left = line.find(num)
+                numLength = len(num)
+            if line.rfind(num) > right:
+                right = line.rfind(num)
+                numLengthR = len(num)
+
+    numberLine += line[left:left+1]
+    numberLine += line[right:right+1]
+    print(line[:left] + Fore.GREEN + line[left:left+numLength] + Fore.RED + line[left+numLength:right] + Fore.GREEN + line[right:right+numLengthR] + Fore.RED + line[right+numLengthR:])
+
+    # if (not numberLine == 'None'):
+    if (len(numberLine)!=2):
+        if ('None' in numberLine):
+            numberLine = numberLine.replace('None','')
+        numberLine*=2
+    if (len(numberLine)==2):
+        # print(numberLine)
+        calibration_values.append(int(numberLine))
+# print(calibration_values)
+total = sum(calibration_values)
+print(Fore.RESET)
+print(total)
